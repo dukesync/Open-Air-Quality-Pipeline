aaf1bcd08ace9d07665fa9c7bc66f00cda861e00
dukesync <theedukesync@gmail.com>
2026-04-11 21:42:19 +0000

    new reports

---
+++ pipeline/open_air_quality/models/marts/reporting/rpt_PM2.5_trends_over_time.sql
@@ -0,0 +1,8 @@
+SELECT 
+    f.measurement_date, 
+    AVG(f.parameter_value) as avg_value
+FROM {{ref('fct_air_quality_measurements') }} f
+JOIN {{ref('dim_parameter') }} p
+ON f.parameter_id = p.parameter_id
+WHERE p.parameter_code = 'pm25'
+GROUP BY 1 ORDER BY 1;
