f222f6bfe13ca53c2232557cf93775c41f1e09e6
dukesync <theedukesync@gmail.com>
2026-04-11 22:12:46 +0000

    fixed syntax

--- pipeline/open_air_quality/models/marts/reporting/rpt_PM2.5_trends_over_time.sql
+++ pipeline/open_air_quality/models/marts/reporting/rpt_PM2.5_trends_over_time.sql
@@ -5,5 +5,4 @@ FROM {{ref('fct_air_quality_measurements') }} f
 JOIN {{ref('dim_parameter') }} p
 ON f.parameter_id = p.parameter_id
 WHERE p.parameter_code = 'pm25'
-GROUP BY 1
-ORDER BY 1;
+GROUP BY 1;
