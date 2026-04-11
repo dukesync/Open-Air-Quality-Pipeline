17f4bea64f503882ed2c29eac553d82de4d9e03c
dukesync <theedukesync@gmail.com>
2026-04-11 22:16:08 +0000

    fixed syntax

--- pipeline/open_air_quality/models/marts/reporting/rpt_air_quality_by_city.sql
+++ pipeline/open_air_quality/models/marts/reporting/rpt_air_quality_by_city.sql
@@ -6,4 +6,4 @@ JOIN {{ref('dim_location') }} l ON f.location_id = l.location_id
 JOIN {{ref('dim_parameter') }} p ON f.parameter_id = p.parameter_id
 WHERE p.parameter_code = 'pm25'
 GROUP BY 1
-ORDER BY 2 ASC;
+ORDER BY 2 ASC
