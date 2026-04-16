3eb5573445d75fbfbc8c84888df8b0deb224ec90
dukesync <theedukesync@gmail.com>
2026-04-16 12:46:20 +0000

    updated new locations

--- pipeline/open_air_quality/models/marts/dim_location.sql
+++ pipeline/open_air_quality/models/marts/dim_location.sql
@@ -13,19 +13,25 @@ select
     case
         when sensor_station ilike '%Delhi%' then 'Reference grade'
         when sensor_station ilike '%Providence Academy%' then 'Air sensor'
-        when sensor_station ilike '%Nakuru%' then 'Air sensor'
+        when sensor_station ilike '%Addis Ababa Central%' then 'Reference grade'
+        when sensor_station ilike '%Kigali_Rwanda%' then 'Air sensor'
+        when sensor_station ilike '%Downtown Manhattan%' then 'Air sensor'
         else 'Other'
     end as sensor_type,
     case
         when sensor_station ilike '%Delhi%' then 'New Delhi'
         when sensor_station ilike '%Providence Academy%' then 'Nairobi'
-        when sensor_station ilike '%Nakuru%' then 'Nakuru'
+        when sensor_station ilike '%Addis Ababa Central%' then 'Addis Ababa'
+        when sensor_station ilike '%Kigali_Rwanda%' then 'Kigali'
+        when sensor_station ilike '%Downtown Manhattan%' then 'New York City'
         else null
     end as city,
     case
         when sensor_station ilike '%Delhi%' then 'India'
         when sensor_station ilike '%Providence Academy%' then 'Kenya'
-        when sensor_station ilike '%Nakuru%' then 'Kenya'
+        when sensor_station ilike '%Addis Ababa Central%' then 'Ethiopia'
+        when sensor_station ilike '%Kigali_Rwanda%' then 'Rwanda'
+        when sensor_station ilike '%Downtown Manhattan%' then 'USA'
         else null
     end as country
 from locs
